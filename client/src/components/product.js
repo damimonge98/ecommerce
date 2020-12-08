60ffadfbdb6071f866ebce69b840b10fbe1c8ddb
ViejaZapata <victorh186@hotmail.com>
2020-12-08 00:54:01 -0300

    componente product

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -3,6 +3,23 @@ import React from 'react';
 const Product = () => {
     return (
         <div className='Products-item'>
+            <img src={require('./images/trueno.jpg')} width='20%'  alt="trueno" /> 
+            <div className='Product-item-info'>
+                <h2> Trueno y Nicki Nicole</h2>
+                <h4>'Mamichula'</h4>
+                <span>
+                    <h4>Precio $ 0.50</h4>
+                    <h5>Stock: disponible</h5>
+                </span>
+                
+                
+            </div>
+            <button type='button'>Comprar</button>
+        </div>
+    );
+}
+
+{/* <div className='Products-item'>
             <img src={props.image} alt={props.title} />
             <div className='Product-item-info'>
                 <h2>{props.title}
@@ -16,8 +33,6 @@ const Product = () => {
                 <p>{props.description}</p>
             </div>
             <button type='button'>Comprar</button>
-        </div>
-    );
-}
+        </div> */}
 
 export default Product;
