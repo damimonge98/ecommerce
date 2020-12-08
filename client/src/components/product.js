0fe69fecaf86d67f657d110764bd93781edffbf0
ViejaZapata <victorh186@hotmail.com>
2020-12-08 11:04:08 -0300

    componente product

--- client/src/components/product.js
+++ client/src/components/product.js
@@ -1,38 +1,25 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({img, title, description, price, stock}) => {
     return (
-        <div className='Products-item'>
-            <img src={require('./images/trueno.jpg')} width='20%'  alt="trueno" /> 
+       <div className='Products-item'>
+            <img src={img} alt={title} />
             <div className='Product-item-info'>
-                <h2> Trueno y Nicki Nicole</h2>
-                <h4>'Mamichula'</h4>
+                <h2>{title}
                 <span>
-                    <h4>Precio $ 0.50</h4>
-                    <h5>Stock: disponible</h5>
+                    $
+                    {' '}
+                    {price}
+                    <p>Stock: {stock}</p>
                 </span>
-                
-                
+                </h2>
+                <p>{description}</p>
             </div>
             <button type='button'>Comprar</button>
-        </div>
+        </div> 
     );
 }
 
-// {/* <div className='Products-item'>
-//             <img src={props.image} alt={props.title} />
-//             <div className='Product-item-info'>
-//                 <h2>{props.title}
-//                 <span>
-//                     $
-//                     {' '}
-//                     {props.price}
-//                     <p>Stock: {props.stock}</p>
-//                 </span>
-//                 </h2>
-//                 <p>{props.description}</p>
-//             </div>
-//             <button type='button'>Comprar</button>
-//         </div> */}
+
 
 export default Product;
