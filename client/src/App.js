79161eed23d1edbfbb337b16aa95015276e62486
Miguel <miguelhernandezjp@gmail.com>
2020-12-08 14:46:12 -0300

    cambios en app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import AddCategories from "./components/AddCategories.jsx";
-import Product from "./components/product.js" 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import AddCategories from "./components/AddCategories.js";
+import AddCategories from "./components/AddCategories/AddCategories.jsx";
+import ProductCard from './components/ProductCard/Product_Card'
+import Product from './components/Product/Product'
+
 
 function App() {
   return (
-    <div>
-    <AddCategories/>
-    <Product/>
-    </div>
+  	<BrowserRouter>
+  	<Route exact path = "/" component = {AddCategories}/>
+    </BrowserRouter>
   );
 }
 
