713c769a39d9c0ee209830823ba42df9ea28d7c3
Miguel <miguelhernandezjp@gmail.com>
2020-12-08 14:48:42 -0300

    cambios app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AddCategories from "./components/AddCategories.js";
-import AddCategories from "./components/AddCategories/AddCategories.jsx";
-import ProductCard from './components/ProductCard/Product_Card'
-import Product from './components/Product/Product'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
+
   	<BrowserRouter>
   	<Route exact path = "/" component = {AddCategories}/>
     </BrowserRouter>
