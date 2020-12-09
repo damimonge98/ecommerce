ae05fef934856fce2837ee5c870d681565ae98cc
Miguel <miguelhernandezjp@gmail.com>
2020-12-09 13:23:11 -0300

    adding delete categories route and get route for testing

--- api/src/routes/routesAddCategories.js
+++ api/src/routes/routesAddCategories.js
@@ -1,6 +1,15 @@
 const server = require('express').Router();
 const { Categories } = require('../db.js');
 
+//agrego el get a categorías para probar la funcionalidad del put y el delete
+server.get('/', (req, res, next) => {
+    Categories.findAll()
+        .then(categories => {
+            res.send(categories);
+        })
+        .catch(next);
+});
+
 server.post("/", (req, res) => {
     const {name, description} = req.body
         Categories.create({
@@ -28,6 +37,18 @@ server.put("/:id", (req,res) => {
 		res.status(201)
 		res.send("Categoria actualizada")
 	})
+})
+
+server.delete(`/:id`, (req, res) => {
+    Categories.destroy(
+		{where: {id: req.params.id}
+    })
+        .then((deletedCategory) => {
+            if (!deletedCategory) {
+                return res.status(404).send({ error: 'No user' });
+            }
+            res.status(200).send('Removed Successfully');
+        });
 })
 
 module.exports = server;
