c51cb7a195f66155899539b8fc1b53e04d79b246
Miguel <miguelhernandezjp@gmail.com>
2020-12-08 22:33:02 -0300

    adding filterId function to be used in the new route /products/:id, that route is in testing for the moment but for now, it works

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import AddCategories from "./components/AddCategories/AddCategories.jsx";
 import ProductCard from './components/ProductCard/Product_Card'
-import Product from './components/Product/Product'
+import {Product, filterId} from './components/Product/Product' 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav.jsx';
 
@@ -11,8 +11,10 @@ function App() {
   	<BrowserRouter>
     <Route exact path = "/" component = {Nav}/>
   	<Route exact path = "/" component = {AddCategories}/>
+    <Route exact path='/products/:id' render={({ match }) =>
+    <Product id={filterId(match.params.id)} />}
+    />
     </BrowserRouter>
-
   );
 }
 
