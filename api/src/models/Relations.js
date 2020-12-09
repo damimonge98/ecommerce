4882eb210514ad253eeb01911029bec2b2dec7bf
v1scount <rodrigovellido1@gmail.com>
2020-12-08 21:58:36 -0300

    Server Up

--- api/src/models/Relations.js
+++
@@ -1,8 +0,0 @@
-import db from '../db'
-import Product from './Product'
-import Categories from './Categories'
-
-Product.belongsToMany(Categories, { through: 'Product_Category'});
-Categories.belongsToMany(Product, { through: 'Product_Category'}); 
-
-export default db;
