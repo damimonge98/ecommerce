144115d132c80890de0956d76215112241e40e56
Agustin Tiseira <tiseiraagustin50@gmail.com>
2020-12-11 08:50:36 -0300

    debugueada

--- client/src/components/Admin/Product/ProductosForm.jsx
+++ client/src/components/Admin/Product/ProductosForm.jsx
@@ -1,4 +1,4 @@
-import React,{Fragment, useState} from 'react';
+import React,{ useState} from 'react';
 import { Multiselect } from 'multiselect-react-dropdown';
 import styles  from './productos-form.module.css';  
 
