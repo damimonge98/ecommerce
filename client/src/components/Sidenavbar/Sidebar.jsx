6df670af7e52588b0d2129926ce73fc85b7c7732
Miguel <miguelhernandezjp@gmail.com>
2020-12-14 01:24:51 -0300

    cambios panel admin

--- client/src/components/Sidenavbar/Sidebar.jsx
+++ client/src/components/Sidenavbar/Sidebar.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
-import {useDispatch} from 'react-redux'
-import {select} from '../../redux/categories'
-
+import { useDispatch } from "react-redux";
+import { select } from "../../redux/categories";
 
 export default function SideBar(props) {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
- 
   return (
     <div>
       <div class="sidebar-container">
@@ -21,41 +19,55 @@ export default function SideBar(props) {
             </Link>
           </li>
           <li>
-            <Dropdown className = 'nav-dropdown'>
-              <Dropdown.Toggle
-                variant=""
-                className="dropdown-basic"
-              >
+            <Dropdown className="nav-dropdown">
+              <Dropdown.Toggle variant="" className="dropdown-basic">
                 <i class="fa fa-headphones-alt" aria-hidden="true" /> Categories
               </Dropdown.Toggle>
-              <Dropdown.Menu className = 'nav-dropdown-list'>
-                <Dropdown.Item onClick={() => dispatch(select('pop'))}>Pop</Dropdown.Item>
-                <Dropdown.Item onClick={() => dispatch(select('rock'))}>Rock</Dropdown.Item>
-                <Dropdown.Item onClick={() => dispatch(select('salsa'))}>Salsa</Dropdown.Item>
-                <Dropdown.Item onClick={() => dispatch(select('cumbia'))}>Cumbia</Dropdown.Item>
-                <Dropdown.Item onClick={() => dispatch(select('rap'))}>Rap</Dropdown.Item>
-                <Dropdown.Item onClick={() => dispatch(select('trap'))}>Trap</Dropdown.Item>
+              <Dropdown.Menu className="nav-dropdown-list">
+                <Dropdown.Item onClick={() => dispatch(select("pop"))}>
+                  Pop
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => dispatch(select("rock"))}>
+                  Rock
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => dispatch(select("salsa"))}>
+                  Salsa
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => dispatch(select("cumbia"))}>
+                  Cumbia
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => dispatch(select("rap"))}>
+                  Rap
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => dispatch(select("trap"))}>
+                  Trap
+                </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </li>
           <li>
-          <Link to={`/`}>
+            <Link to={`/`}>
               <i class="fa fa-user" aria-hidden="true"></i> Log In
             </Link>
           </li>
           <li>
-          <Link to={`/`}>
+            <Link to={`/`}>
               <i class="fa fa-shopping-cart" aria-hidden="true"></i> Shopping
               Cart
             </Link>
           </li>
           <li>
-          <Link to={`/`}>
+            <Link to={`/`}>
               <i class="fa fa-ticket-alt" aria-hidden="true"></i> Concerts
             </Link>
           </li>
           <li>
-          <Link to={`/`}>
+            <Link to={`/admin`}>
+              <i class="fa fa-user-cog" aria-hidden="true"></i> Admin
+            </Link>
+          </li>
+          <li>
+            <Link to={`/`}>
               <i class="fa fa-info-circle" aria-hidden="true"></i> Information
             </Link>
           </li>
