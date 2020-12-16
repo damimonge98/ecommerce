236c14aa2ee8bfd6d874ec4fda383befa1ae22f5
Damian <damimonge98@gmail.com>
2020-12-16 14:30:21 -0300

    conexion con back de addcategory

--- client/src/redux/reducers/categoriesReducer.js
+++ client/src/redux/reducers/categoriesReducer.js
@@ -1,1 +1,26 @@
+import {
+    AGREGAR_CATEGORIA
+ } from "../types/categories";
 
+
+//cada reducer tiene su propio state
+const initialState = {
+    categorias: [],
+    error: null,
+    loading:false,
+    productoeliminar: null,
+    productoeditar:null
+}
+
+
+export default function(state = initialState, action){
+    switch(action.type){
+        case AGREGAR_CATEGORIA:
+            return {
+                ...state,
+                categorias: action.payload
+            }
+        default:
+            return state;
+    }
+}
