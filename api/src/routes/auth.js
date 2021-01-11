b4c16923dcd7c9d5a21f7cb3dc3f66fae99fcfd1
Miguel <miguelhernandezjp@gmail.com>
2021-01-10 21:13:54 -0300

    ruta promote

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,27 +1,27 @@
 const server = require('express').Router();
-const  { User } = require('../db.js');
+const { User } = require('../db.js');
 const cors = require('cors')
 const passport = require('passport');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 server.use(cors());
 require('../passport-config')(passport);
 
 //login
-server.post('/', (req, res,next) => {
-	
-	passport.authenticate("local",{session:false},(err,user,info)=>{
-		if(err) throw err;
-		if(!user) res.status(400).json({msg:'Email o Password incorrecto'})
-		else{
-			req.login(user,{session:false},err=>{
-				if(err) throw err;
+server.post('/', (req, res, next) => {
+
+	passport.authenticate("local", { session: false }, (err, user, info) => {
+		if (err) throw err;
+		if (!user) res.status(400).json({ msg: 'Email o Password incorrecto' })
+		else {
+			req.login(user, { session: false }, err => {
+				if (err) throw err;
 				//crear y asignar un token
-				const body = { id: user.id,username : user.username , email: user.email, isAdmin: user.isAdmin, givenName: user.givenName, familyName: user.familyName,photoURL: user.photoURL }
-				const token = jwt.sign({user:body},process.env.TOKEN_SECRET);
-				res.header('auth-token',token).json({token});
+				const body = { id: user.id, username: user.username, email: user.email, isAdmin: user.isAdmin, givenName: user.givenName, familyName: user.familyName, photoURL: user.photoURL }
+				const token = jwt.sign({ user: body }, process.env.TOKEN_SECRET);
+				res.header('auth-token', token).json({ token });
 			});
 		}
-	})(req,res,next);	
+	})(req, res, next);
 });
 
 
@@ -33,8 +33,20 @@ server.get('/auth/facebook', passport.authenticate('facebook'));
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
 server.get('/auth/facebook/login_fb',
-  passport.authenticate('facebook', { successRedirect: '/',
-									  failureRedirect: '/login' }));
+	passport.authenticate('facebook', {
+		successRedirect: '/',
+		failureRedirect: '/login'
+	}));
 
-									  
+// Ruta para actualizar el rol de un usuario, para hacerlo administrador o sacarle ese rol
+
+server.put('/auth/promote/:id', passport.authenticate("jwt", { session: false }),(req, res) => {
+	const { isAdmin } = req.body;
+	User.update({ isAdmin },
+		{ returning: true, where: { id: req.params.id } })
+		.then(userPromoted => {
+			res.send(userPromoted)
+		})
+})
+
 module.exports = server
